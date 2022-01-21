c89993d257fafa023f1b8dd4df017b2a66e69ab3
imtanvir <tanvirparvej101@gmail.com>
2022-01-21 15:11:36 +0600

    Import the program files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leep_Year_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            /*if (year % 400 == 0 || (Convert.ToString(year).Length != 4 && year % 4 == 0))*/
+            if((year % 400 == 0 && year % 100 != 0) || (year % 100 != 0 && year % 4 == 0))
+            {
+                Console.WriteLine("Leep Year");
+            }
+            else
+            {
+                Console.WriteLine("Not a leep year");
+            }
+            {
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
